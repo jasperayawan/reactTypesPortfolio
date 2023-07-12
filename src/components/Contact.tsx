2688e1b9ccb985c6b8cdc2ee8aed760e1cf484ac
jasperayawan <ejayawan22@gmail.com>
2023-07-12 23:12:43 +0800

    update contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get in touch</h2>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -71,7 +71,7 @@ export default function Contact() {
         </div>
         <div className="mt-10">
           <button
-            type="submit"
+            
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Let's talk
